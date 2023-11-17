c59cb5bb3b29bae4ce35bdba23fb94b0f2bb450f
GabrielDelMonte <gabrieldmsn@hotmail.com>
2023-11-17 01:31:43 -0300

    feat: new files

---
+++ src/model/FaceTracker.py
@@ -0,0 +1,13 @@
+import time
+import math
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
+
+class FaceTracker:
+    pass
